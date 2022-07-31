850a50b2586db8aa677ac038a1ad3d6c903acf07
Pooyan Khanjankhani <p.khani@tejaratshayan.com>
2022-07-31 14:34:23 +0430

    feat(executor): add `HttpExecutor`

---
+++ src/executor/http-executor.ts
@@ -0,0 +1,41 @@
+import { Executor } from './types/executor';
+import { VariableStorage } from 'src/variable-storage/variable-storage';
+import { HttpCommandFactory } from 'src/commands/http/http-command-factory';
+import { HttpResourceService } from 'src/resources/http/http-resource-service';
+import { HttpResource, Resources } from 'src/types/transaction-declaration';
+
+export class HttpExecutor implements Executor {
+  constructor(
+    private readonly service: HttpResourceService,
+    private readonly storage: VariableStorage,
+    private readonly resources: Resources<HttpResource>,
+    private readonly factory: HttpCommandFactory,
+  ) {}
+
+  async start() {
+    const command = await this.factory.create(
+      this.storage,
+      this.resources.start,
+    );
+
+    return this.service.call(command);
+  }
+
+  async commit() {
+    const command = await this.factory.create(
+      this.storage,
+      this.resources.commit,
+    );
+
+    return this.service.call(command);
+  }
+
+  async rollback() {
+    const command = await this.factory.create(
+      this.storage,
+      this.resources.commit,
+    );
+
+    return this.service.call(command);
+  }
+}
