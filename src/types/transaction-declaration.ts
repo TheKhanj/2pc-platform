abab33c4ac7a64ee5bf37e0af8d6f5d1b657f93b
Pooyan Khanjankhani <p.khani@tejaratshayan.com>
2022-07-31 14:33:03 +0430

    refactor(): update core types

--- src/types/transaction-declaration.ts
+++ src/types/transaction-declaration.ts
@@ -1,35 +1,41 @@
-import { Expression, StringExpression } from './expression';
+import { Method } from 'axios';
+
+import { Expression, StringExpression } from './expressions';
 
 export type TypeDefinition = any;
 
-export type ResourceDeclaration =
-  | {
-      type: 'http';
-      headers: Record<StringExpression, StringExpression>;
-      body: Expression;
-      params: Record<StringExpression, StringExpression>;
-      queries: Record<StringExpression, StringExpression>;
-    }
-  | {
-      type: 'rabbitmq';
-    };
-
-export type Resources = {
-  start: ResourceDeclaration;
-  commit: ResourceDeclaration;
-  rollback: ResourceDeclaration;
+export type HttpResource = {
+  type: 'http';
+  method: Method;
+  url: string;
+  headers: Record<StringExpression, StringExpression>;
+  body: Expression;
+  params: Record<StringExpression, StringExpression>;
+  queries: Record<StringExpression, StringExpression>;
 };
 
-export type StateDeclaration = {
+export type RabbitMQResource = {
+  type: 'rabbitmq';
+};
+
+export type Resource = HttpResource | RabbitMQResource;
+
+export type Resources<R = Resource> = {
+  start: R;
+  commit: R;
+  rollback: R;
+};
+
+export type State = {
   name: string;
   type: 'task';
   result: TypeDefinition;
-  resources: Resources[];
+  resources: Resources;
 };
 
-export type TransactionDeclaration = {
+export type Transaction = {
   id: any;
   name: string;
   status: 'active' | 'inactive';
-  states: StateDeclaration[];
+  states: State[];
 };
