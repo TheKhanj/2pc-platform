aa235a4195fc78181dc3ea07f92f3ab4f6a12dde
Pooyan Khanjankhani <p.khani@tejaratshayan.com>
2022-07-31 15:54:16 +0430

    feat(session): add abstract `Session`

---
+++ src/session/abstract/session.ts
@@ -0,0 +1,36 @@
+import { Logger } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+
+import { Executor } from 'src/executor/types/executor';
+import { VariableStorage } from 'src/variable-storage/variable-storage';
+
+export abstract class Session<R> {
+  protected readonly id: string;
+
+  constructor(
+    protected readonly executors: Executor[],
+    protected readonly storage: VariableStorage,
+  ) {
+    this.id = uuid();
+  }
+
+  public async start(input: Record<string, any>): Promise<R> {
+    try {
+      Logger.warn('Input field is not set in storage', 'Session');
+
+      const res = await this._start();
+      await this.commit();
+
+      return res;
+    } catch (err) {
+      await this.rollback();
+      throw err;
+    }
+  }
+
+  protected abstract _start(): Promise<R>;
+
+  protected abstract commit(): Promise<void>;
+
+  protected abstract rollback(): Promise<void>;
+}
