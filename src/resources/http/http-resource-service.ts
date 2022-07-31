eeffc23ec1a57c2840e23e97265024a0052cf861
Pooyan Khanjankhani <p.khani@tejaratshayan.com>
2022-07-31 13:16:31 +0430

    feat(resource): add `HttpResourceService`

---
+++ src/resources/http/http-resource-service.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { Logger } from '@nestjs/common';
+
+import { HttpResult } from 'src/results/http-result';
+import { HttpCommand } from 'src/commands/http-command';
+import { ResourceService } from '../types/resource-service';
+
+export class HttpResourceService
+  implements ResourceService<HttpCommand, HttpResult> {
+  async call<T = any>(command: HttpCommand): Promise<HttpResult<T>> {
+    const res = await axios.request({
+      method: command.method,
+      url: this.parseUrl(command.url, command.params),
+      params: command.queries,
+      data: command.body,
+    });
+
+    return {
+      status: res.status,
+      headers: res.headers,
+      body: res.data,
+    };
+  }
+
+  private parseUrl(url: string, params: Record<string, string>) {
+    Logger.warn('Url parsing is not implemented yet', 'UrlParser');
+    return url;
+  }
+}
