e12c74dced122da03728c86a213676a519ddb8ab
Pooyan Khanjankhani <p.khani@tejaratshayan.com>
2022-08-01 11:06:32 +0430

    feat(): add `HttpMethod` enum

---
+++ src/types/http-method.ts
@@ -0,0 +1,12 @@
+export enum HttpMethod {
+  GET = 'GET',
+  DELETE = 'DELETE',
+  HEAD = 'HEAD',
+  OPTIONS = 'OPTIONS',
+  POST = 'POST',
+  PUT = 'PUT',
+  PATCH = 'PATCH',
+  PURGE = 'PURGE',
+  LINK = 'LINK',
+  UNLINK = 'UNLINK',
+}
