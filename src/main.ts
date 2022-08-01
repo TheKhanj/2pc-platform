b9b6656581914295f57e87e28f501f0aa22f717c
Pooyan Khanjankhani <p.khani@tejaratshayan.com>
2022-08-01 10:15:22 +0430

    feat(bootstrap): build application using `ApplicationBuilder`

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ApplicationBuilder, ApplicationDirector } from '@shayan/common';
+
+import { AppModule } from './app-module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const builder = new ApplicationBuilder(app, app.select(AppModule));
+  const director = new ApplicationDirector(builder);
+  await director.create({
+    enableGlobalPermissionInterceptor: false,
+    swagger: {},
+  });
+
+  await app.listen(process.env.PORT || '3000');
 }
 bootstrap();
