7752aa522e113c3ce11c49cbfefe1c72be7dc7bb
Pooyan Khanjankhani <p.khani@tejaratshayan.com>
2022-08-01 11:27:40 +0430

    feat(@config): inject mongoose document in `ConfigService`

--- src/submodules/config/service/config-service.ts
+++ src/submodules/config/service/config-service.ts
@@ -1,4 +1,13 @@
+import * as mongoose from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { ConfigModel } from '../models/config-model';
 
 @Injectable()
-export class ConfigService {}
+export class ConfigService {
+  constructor(
+    @InjectModel(ConfigModel.name)
+    private readonly model: mongoose.Document<ConfigModel>,
+  ) {}
+}
