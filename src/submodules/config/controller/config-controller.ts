c1146f5f52979f72114a140ca9610a5255f6efe7
Pooyan Khanjankhani <p.khani@tejaratshayan.com>
2022-08-01 13:13:54 +0430

    feat(@config): implement `ConfigController`

--- src/submodules/config/controller/config-controller.ts
+++ src/submodules/config/controller/config-controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  NotImplementedException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+
+import { ConfigIdDto } from '../dto/config-id-dto';
+import { ConfigService } from '../service/config-service';
+import { CreateConfigDto } from '../dto/create-config-dto';
 
 @Controller('/config')
-export class ConfigController {}
+export class ConfigController {
+  constructor(private readonly service: ConfigService) {}
+
+  @Get('/:configId/getById')
+  async getById(@Param() params: ConfigIdDto) {
+    const res = await this.service.getById(params.configId);
+    return res;
+  }
+
+  @Post('/create')
+  async create(@Body() config: CreateConfigDto) {
+    const res = await this.service.create(config);
+
+    return res;
+  }
+
+  @Put('/:configId/updateById')
+  async update(@Param() params: ConfigIdDto) {
+    throw new NotImplementedException();
+  }
+
+  @Delete('/:configId/deleteById')
+  async delete(@Param() params: ConfigIdDto) {
+    await this.service.delete(params.configId);
+  }
+}
