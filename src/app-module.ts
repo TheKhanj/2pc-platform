b2c6cf283ee18a0e29d686b1b9508b984e2f1933
Pooyan Khanjankhani <p.khani@tejaratshayan.com>
2022-08-01 11:25:48 +0430

    feat(): add mongoose connection in root module

--- src/app-module.ts
+++ src/app-module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { ConfigModule } from './submodules/config/config-module';
 
+const dbUrl = [
+  process.env.DB_HOST,
+  process.env.DB_PORT,
+  process.env.DB_NAME,
+].reduce((ret, value) => {
+  return ret?.replace(/\%[^\%]+\%/, value || '');
+}, 'mongodb://%host%:%port%/%database%') as string;
+
 @Module({
-  imports: [ConfigModule],
+  imports: [MongooseModule.forRoot(dbUrl), ConfigModule],
 })
 export class AppModule {}
