185abf8e391401d8900c195bd01ad71aa8dcbd0a
Pooyan Khanjankhani <p.khani@tejaratshayan.com>
2022-08-01 13:13:04 +0430

    feat(@config): add `IsResource` decorator

---
+++ src/submodules/config/decorators/is-resource.ts
@@ -0,0 +1,16 @@
+import { Type } from 'class-transformer';
+
+import { BaseResource } from 'src/types/transaction-declaration';
+import { BaseResourceDto } from '../dto/base-resource-dto';
+
+export function IsResource(
+  resources: { type: BaseResource['type']; dto: any }[],
+): PropertyDecorator {
+  return Type(() => BaseResourceDto, {
+    keepDiscriminatorProperty: true,
+    discriminator: {
+      property: 'type',
+      subTypes: resources.map((res) => ({ name: res.type, value: res.dto })),
+    },
+  });
+}
