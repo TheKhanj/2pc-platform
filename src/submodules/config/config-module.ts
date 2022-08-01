44b77080b38871420b0aa858ebaf79ba5b56cd9a
Pooyan Khanjankhani <p.khani@tejaratshayan.com>
2022-08-01 11:27:01 +0430

    feat(@config): inject mongoose in `ConfigModule`

--- src/submodules/config/config-module.ts
+++ src/submodules/config/config-module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { ConfigService } from './service/config-service';
 import { ConfigController } from './controller/config-controller';
+import { ConfigModel, ConfigSchema } from './models/config-model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: ConfigModel.name,
+        schema: ConfigSchema,
+      },
+    ]),
+  ],
   controllers: [ConfigController],
   providers: [ConfigService],
 })
