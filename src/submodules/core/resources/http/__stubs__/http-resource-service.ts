f66d7150df56fe4c6ef472b444796416f0d7cb5f
Pooyan Khanjankhani <p.khani@tejaratshayan.com>
2022-08-06 12:41:37 +0430

    test(@core): add `mockHttpResource`

---
+++ src/submodules/core/resources/http/__stubs__/http-resource-service.ts
@@ -0,0 +1,18 @@
+import { HttpResult } from 'src/submodules/core/results/http-result';
+import { HttpCommand } from 'src/submodules/core/commands/http/http-command';
+import { ResourceService } from 'src/submodules/core/resources/types/resource-service';
+
+export const mockHttpResourceService: ResourceService<
+  HttpCommand,
+  HttpResult
+> = {
+  call: jest.fn(async () => {
+    return {
+      status: 200,
+      headers: {
+        header1: 'RESULT_HEADER_1',
+      },
+      body: 'RESULT_BODY',
+    };
+  }),
+};
